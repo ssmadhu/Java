ccbe9018e83cee03a524eba0f238c1cc00372dd3
Akshay <asharma95@aol.com>
2020-03-23 13:04:57 -0400

    first commit

--- src/main/java/org/javaCourse/App.java
+++ src/main/java/org/javaCourse/App.java
@@ -15,7 +15,7 @@ public class App
 {
     public static List<User> users = new ArrayList<User>();
     public static List<Course> courses = new ArrayList<Course>();
-    public static void main( String[] args )
+    public static void main(String[] args)
     {
 
 
@@ -70,6 +70,54 @@ public class App
     }
 
     public static void displayUserOptions() {
+        System.out.println("Please enter Username: " );
+        Scanner scan = new Scanner(System.in);
+        String userName=scan.next();
 
+        Boolean bool=null;
+        int index = 0;
+        for (int i = 0; i < users.size(); i++){
+            if(users.get(i).getName() == userName){
+                bool=true;
+                index=i;
+                break;
+            }
+        }
+        if (bool){
+            System.out.println("1. View Courses and Buy  \n 2. View Purchase History \n Enter Choice: " );
+            scan = new Scanner(System.in);
+            int choice = scan.nextInt();
+
+            switch(choice) {
+                case 1: {
+                    for (int i=0;i<courses.size();i++){
+                        System.out.println("ID:"+courses.get(i).getId()+ " " + "Course Name:" +courses.get(i).getName() + "\n" );
+                    }
+                    System.out.println("Please enter ID of the Course you would like to buy. Enter -1 to buy nothing. " );
+                    scan = new Scanner(System.in);
+                    int id = scan.nextInt();
+                    if (choice>-1){
+                        for (int i = 0; i < courses.size(); i++){
+                            if(id == courses.get(i).getId()){
+                                users.get(index).addCoursesPurchased(courses.get(i));
+                                users.get(index).setBalance(users.get(index).getBalance()-courses.get(i).getCost());
+                                break;
+                            }
+                        }
+                    }
+                }
+                case 2: {
+                    List<Course> coursePurchased = users.get(index).getCoursesPurchased();
+                    for (int i=0; i<coursePurchased.size(); i++){
+                        System.out.println("ID:"+ coursePurchased.get(i).getId() + " " + "Course Name:" + coursePurchased.get(i).getName() + "\n" );
+                    }
+                }
+                default: {
+                    System.out.println("Enter a valid choice!!");
+                }
+            }
+        } else {
+            System.out.println("User Name was not found.");
+        }
     }
 }
